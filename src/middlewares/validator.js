4354039167b380ec8f761bc432d387cff138971d
Informatica <informatica@alumno.local>
2025-02-04 11:51:58 -0600

    se agrego crud completo error en eliminar

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -8,15 +8,13 @@ export const registerValidator = [
     body("email","The email must be valid").isEmail(),
     body("role").custom(esRoleValido),
     body("email").custom(existenteEmail),
-    body("pasword","The pasword must be at least 6 characters").isLength({ min: 8}),
-    validarCampos
+    body("password", "La contraseña debe tener minimo 8 caracteres").isLength({min: 8}),    validarCampos
 ] ;
 
 export const loginValidator = [
     body("email").optional().isEmail().withMessage("Enter a valid email address"),
     body("username").optional().isString().withMessage("Enter a valid username"),
-    body("pasword","The pasword must be at least 6 characters").isLength({min: 8}),
-    validarCampos
+    body("password", "La contraseña debe tener minimo 8 caracteres").isLength({min: 8}),    validarCampos
 ]
 
 
